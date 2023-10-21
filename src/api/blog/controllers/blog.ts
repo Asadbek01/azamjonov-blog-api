701439f0d6c9587e7c1c8a2e51b475d35cb051c5
Asadbek_Azamjonov <asadbek.azamjonov@devkom.pl>
2023-10-21 20:09:55 +0200

    initial commit

--- src/api/blog/controllers/blog.ts
+++ src/api/blog/controllers/blog.ts
@@ -1,30 +1,31 @@
-'use strict';
-
 /**
  * blog controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
-const { sanitizeOutput } = require('strapi-utils');
-
-function transformResponse(entity: any) {
-    // do some transformation on the entity
-    return entity;
-}
 
 module.exports = createCoreController('api::blog.blog', ({ strapi }) => ({
-    async findOne(ctx: any) {
+    async findOne(ctx) {
         const { id } = ctx.params;
 
         const entity = await strapi.db.query('api::blog.blog').findOne({ 
             where: { slug: id },    
             populate: ['thumbnail'],
-         });
-        const sanitizedEntity = await sanitizeOutput(entity, strapi.models['blog'], ctx);
+        });
 
-        return transformResponse(sanitizedEntity);
+        const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
 
-        },
+        return this.transformResponse(sanitizedEntity);
+    },
 
+    sanitizeOutput(entity, ctx) {
+        // Define your sanitization logic here
+        return entity;
+    },
+
+    transformResponse(sanitizedEntity) {
+        // Define your response transformation logic here
+        return sanitizedEntity;
+    },
 }));
-
+// 
